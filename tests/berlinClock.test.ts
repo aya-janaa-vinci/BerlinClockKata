c5dcd4da2eb514503c7b2d18c359263a44bd593d
aya-janaa-vinci <aya.janaa@student.vinci.be>
2026-01-15 18:08:07 +0100

    add: single minutes one yellow when input is one minute

--- tests/berlinClock.test.ts
+++ tests/berlinClock.test.ts
@@ -2,20 +2,20 @@ import { BerlinClock } from "../src/berlinClock";
 
 describe("BerlinClock", () => {
   describe("translate single minute", () => {
-    it("should return XXXX when given 00:00:00", () => {
-      let berlinClock = new BerlinClock();
+    let berlinClock = new BerlinClock();
 
+    it("should return XXXX when given 00:00:00", () => {
+    
       const actual = berlinClock.translateSingleMinutes("00:00:00");
 
       expect(actual).toBe("XXXX"); //X représente la lampe
     });
 
     it("should return JXXX when given 00:01:00", () => {
-        let berlinClock = new BerlinClock();
 
-        const actual = berlinClock.translateSingleMinutes("00:01:00")
+      const actual = berlinClock.translateSingleMinutes("00:01:00");
 
-        expect(actual).toBe("JXXX");
-    })
+      expect(actual).toBe("JXXX");
+    });
   });
 });
